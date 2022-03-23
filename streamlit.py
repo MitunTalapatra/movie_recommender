78b8955f741585c7ae1d7b17b381f00d4fe9e2c5
MitunTalapatra <mitun_urp@yahoo.com>
2022-03-23 09:49:35 +0100

    push

--- streamlit.py
+++ streamlit.py
@@ -23,7 +23,7 @@ dictionary = pd.read_csv('data/cleaned_movies_dictionary.csv', index_col=[0])
 # Write a title
 st.title('Movie Recommender')
 # Write data taken from https://allisonhorst.github.io/palmerpenguins/
-st.write('**Little** *app* for finding a good movie to watch [datset](https://grouplens.org/datasets/movielens/)')
+st.write('**My** **app** for finding a good movie to watch [dataset](https://grouplens.org/datasets/movielens/)')
 # Put image https://raw.githubusercontent.com/allisonhorst/palmerpenguins/master/man/figures/lter_penguins.png
 st.image('https://images.app.goo.gl/Qt3ziMxz2GxzvmbB9')
 st.write('Please enter 3 movies you have watched and rate them from 0-5')
