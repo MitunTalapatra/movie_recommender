505d0b232f6aba795503e76d7e73b7b6fc7e3797
MitunTalapatra <mitun_urp@yahoo.com>
2022-03-23 13:45:24 +0100

    push

--- streamlit.py
+++ streamlit.py
@@ -25,7 +25,7 @@ st.title('Movie Recommender')
 # Write data taken from https://allisonhorst.github.io/palmerpenguins/
 st.write('**My** **app** for finding a good movie to watch [dataset](https://grouplens.org/datasets/movielens/)')
 # Put image https://raw.githubusercontent.com/allisonhorst/palmerpenguins/master/man/figures/lter_penguins.png
-st.image('https://media1.giphy.com/media/aImJnc9F8Omzu/100.webp?cid=ecf05e478ysj7twzfeoblr8zouuk7lp24li5sbgad2njfpvz&rid=100.webp&ct=g')
+st.image(https://c.wallhere.com/photos/f5/92/1920x1080_px_Inception_movies-630272.jpg!d')
 st.write('Please enter 3 movies you have watched and rate them from 0-5')
 
 input_1 = st.text_input('Movie 1', 'A Beautiful Mind'); rating_1= st.slider('Rate movie 1', 0.0, 5.0, (0.0))
